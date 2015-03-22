a3917f8aea9bde376b3d532be48adc200e699b26
Sushant Mane <sushantmane7@gmail.com>
2015-03-22 20:14:41 +0530

    Add validations for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :title, presence: true,
+                    uniqueness: true
+  validates :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: {
+                      with: %r{\.(gif|jpg|png)\Z}i,
+                      meassage: 'must be URL for GIF, JPG or PNG image.'
+                      }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,63 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title:        'This is product title',
+                          description:  'This is product description',
+                          image_url:    'product_image_url.jpg')
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title:        'This is product title',
+                description:  'This is description of product',
+                price:        1,
+                image_url:    image_url)
+  end
+
+  test 'image url' do
+    ok = %w{ thumb.gif thumb.jpg thumb.png THUMB.GIF thumb.jpg thumb.PNG
+             http://www.samkart.com/product/images/product_tumbnail.gif }
+
+    bad = %w{  thumb.doc thumb.jpg/img thumb.img thumb.png.img }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{ name } must be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{ name } must not be valid"
+    end
+  end
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(title:        products(:saree).title,
+                          description: 'product desc',
+                          price:        1,
+                          image_url:  'stylus-series-black-semi-stitched-gown.jpg')
+
+    assert product.invalid?
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
 end
+
