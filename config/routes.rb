00af19726600d354852669582e982c5498cca24f
Sushant Mane <sushantmane7@gmail.com>
2015-03-23 11:56:36 +0530

    Add Store controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get 'store/index'
   resources :products
+
+  root 'store#index', as: 'store'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
